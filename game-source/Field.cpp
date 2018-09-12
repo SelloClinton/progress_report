c8201ef9d1c1a5c8e0522a0f7e38d397a2d23abc
SelloClinton <680492@students.wits.ac.za>
2018-09-12 16:35:25 +0200

    total number of mushrooms displayed. had to take srand() out of loops

--- game-source/Field.cpp
+++ game-source/Field.cpp
@@ -22,6 +22,7 @@ void Field::createMushrooms(int numberOfMushrooms){		//this causes some delay wh
 		while((x_iter != end(xPositions_))&&(y_iter != end(yPositions_))){
 			auto x_position = *x_iter;
 			auto y_position = *y_iter;
+			cout << "x : " << x_position << "  y : " << y_position << endl;
 			auto position = make_shared<Position>(x_position,y_position);
 			auto mushroom = make_shared<Mushroom>(position);
 			mushrooms_.push_back(mushroom);
@@ -42,16 +43,18 @@ void Field::generateXPositions(int numberOfMushrooms){
 	
 
 	auto validPosition = 0;
-	
+	srand(time(NULL));
 	while(validPosition != numberOfMushrooms){
-			srand(time(NULL));
+		
+
 			auto random_multiple_of_four = rand() %744+1;
 			
 			if (random_multiple_of_four%4 == 0){
 					xPositions_.push_back(random_multiple_of_four);
 					validPosition++;
 			}
-				
+//		xPositions_.push_back(0);
+//		validPosition++;				
 	}
 //	cout << "xPositions size : " << xPositions_.size() << endl;
 }
@@ -59,37 +62,36 @@ void Field::generateXPositions(int numberOfMushrooms){
 void Field::generateYPositions(int numberOfMushrooms){
 	
 	//corresponding multiples are yet to be discovered
-
-	
+	srand(time(NULL));
 	for(auto i = 0; i != numberOfMushrooms; i++){
-		srand(time(NULL));
+
 		auto random_multiple_of_four = rand() %(Constants::DISPLAY_HEIGHT_-50)+1;
 		yPositions_.push_back(random_multiple_of_four);
 	}
 //	cout << "Ypositions size: " << yPositions_.size() << endl;
 }
 
-void Field::createPositions(){
-		
-	
-		auto x_iter =  begin(xPositions_);
-		auto y_iter = begin(yPositions_);
-		
-		while(x_iter != end(xPositions_)){
-			auto x_position = *x_iter;
-			auto y_position = *y_iter;
-			auto position = make_shared<Position>(x_position,y_position);
-			x_iter++;
-			y_iter++;
-		}
-	
-//	for(auto x_iter = begin(xPositions_), y_iter = begin(yPositions_); x_iter != end(xPositions_), y_iter != end(yPositions_); x_iter++,y_iter++){
+//void Field::createPositions(){
 //		
+//	
+//		auto x_iter =  begin(xPositions_);
+//		auto y_iter = begin(yPositions_);
+//		
+//		while(x_iter != end(xPositions_)){
 //			auto x_position = *x_iter;
 //			auto y_position = *y_iter;
 //			auto position = make_shared<Position>(x_position,y_position);
-//			positions_.push_back(position);
 //			x_iter++;
 //			y_iter++;
-//	}
-}
+//		}
+//	
+////	for(auto x_iter = begin(xPositions_), y_iter = begin(yPositions_); x_iter != end(xPositions_), y_iter != end(yPositions_); x_iter++,y_iter++){
+////		
+////			auto x_position = *x_iter;
+////			auto y_position = *y_iter;
+////			auto position = make_shared<Position>(x_position,y_position);
+////			positions_.push_back(position);
+////			x_iter++;
+////			y_iter++;
+////	}
+//}
